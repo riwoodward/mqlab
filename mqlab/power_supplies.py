3062cf05f2ef5a953ee9366f75029ee3c77cc246
Robert Woodward <r.i.woodward@gmail.com>
2017-09-25 15:10:04 +1000

    new power supply features

--- mqlab/power_supplies.py
+++ mqlab/power_supplies.py
@@ -2,16 +2,17 @@
 from __future__ import division, print_function, absolute_import, unicode_literals
 from builtins import ascii, bytes, chr, dict, filter, hex, input, int, map, next, oct, open, pow, range, round, str, super, zip
 
+import time
 from mqlab.connections import Instrument
 
 
 class PowerSupply(Instrument):
 
-    def __init__(self, max_currrent_A=1000, **kwargs):
+    def __init__(self, max_current_A=1000, **kwargs):
         """ Init for power supply including a safety routine to limit accidental setting of erroneously high currents. """
 
         # Store current limit
-        self.max_currrent_A = max_currrent_A
+        self.max_current_A = max_current_A
 
         # Init main MQ instrument class
         super().__init__(**kwargs)
@@ -22,8 +23,8 @@ class HP6653A(PowerSupply):
 
     def set_current(self, current):
         """ Set the current limit to the user specified current (A) maintaining all other settings. """
-        if current > self.max_currrent_A:
-            raise ValueError('Entered current value [{} A] is above the device max current limit [{} A]. Check input and raise the current limit when initialising the device connection if needed.'.format(current, self.max_currrent_A))
+        if current > self.max_current_A:
+            raise ValueError('Entered current value [{} A] is above the device max current limit [{} A]. Check input and raise the current limit when initialising the device connection if needed.'.format(current, self.max_current_A))
         else:
             self.send('CURR {:.3f}'.format(current))
 
@@ -53,8 +54,8 @@ class Newport5600(PowerSupply):
 
     def set_current(self, current):
         """ Set the current limit to the user specified current (A) maintaining all other settings. """
-        if current > self.max_currrent_A:
-            raise ValueError('Entered current value [{} A] is above the device max current limit [{} A]. Check input and raise the current limit when initialising the device connection if needed.'.format(current, self.max_currrent_A))
+        if current > self.max_current_A:
+            raise ValueError('Entered current value [{} A] is above the device max current limit [{} A]. Check input and raise the current limit when initialising the device connection if needed.'.format(current, self.max_current_A))
         else:
             self.send('LASer:LDI {:.3f}'.format(current))
 
@@ -77,6 +78,20 @@ class Newport5600(PowerSupply):
     def set_output_on(self):
         """ Enable output. """
         self.send('LASer:OUTput 1')
+
+    def current_kick(self, low_value, high_value, delay=1):
+        """ Set current to low value for a given delay, then jump back up to high value - useful for kick-starting mode-locking! """
+        self.set_current(low_value)
+        time.sleep(delay)
+        self.set_current(high_value)
+
+    def multiple_current_kicks(self, low_value, high_values, delay=1, delay_between_kicks=5):
+        for high_value in high_values:
+            self.set_current(low_value)
+            time.sleep(delay)
+            print('{:.2f} A'.format(high_value))
+            self.set_current(high_value)
+            time.sleep(delay_between_kicks)
 
     # Create virtual entities for current and votlage so they can be simply accessed as variables (e.g. self.current=1) rather than using setters / getters
     current = property(get_current, set_current)
